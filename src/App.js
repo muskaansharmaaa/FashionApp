f289a7c2a20e7ba74c2f752de902e97c45c2ed05
muskaansharmaaa <54302217+muskaansharmaaa@users.noreply.github.com>
2020-05-25 22:08:51 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    
+    
+    <div className="Hey">
+    <center><h1 color="white">WELCOME!</h1></center>
+   
+    </div>
+  
+  
+
+
+  );
+}
+
+export default App;
+
+
+
